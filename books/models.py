46d7bb6b5f96e498307a1730336b41dfed968bc2
Tamanna <tamannakhanum@hotmail.co.uk>
2024-01-12 12:07:51 +0000

    "Created a books folder. Added Book model. Updated settings."

---
+++ books/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+  title = models.CharField(max_length=300)
+  author = models.CharField(max_length=300)
+  publication_date = models.PositiveIntegerField()
+  genres = models.CharField(max_length=300)
+  image = models.CharField(max_length=1000)
+  owner = models.ForeignKey(
+    to='users.User',
+    on_delete=models.CASCADE,
+    related_name='owned_books',
+    null=True
+  )
+
+  def __str__(self):
+    return f'{self.title} ({self.publication_date}) - Author: {self.author}'
