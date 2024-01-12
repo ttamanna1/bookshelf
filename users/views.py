ec74fbf7235ed9f12f5162dffeaac0ecde42301a
Tamanna <tamannakhanum@hotmail.co.uk>
2024-01-12 12:48:47 +0000

    "Added user registration."

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from .serializers.common import RegistrationSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your views here.
+class RegisterView(CreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = RegistrationSerializer
