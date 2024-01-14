572379430ff5b43bcf3677e98222285c9bb84ccb
Tamanna <tamannakhanum@hotmail.co.uk>
2024-01-14 15:41:06 +0000

    "Defining currently reading and finished components."

--- client/src/components/CurrentlyReading.jsx
+++ client/src/components/CurrentlyReading.jsx
@@ -1,1 +1,37 @@
-export default function CurrentlyReading() {}
+import { useLoaderData } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
+
+export default function CurrentlyReading() {
+  const booklist = useLoaderData()
+
+  //! State
+  const [books, setBooks] = useState(booklist || [])
+  
+
+  const handleMoveToCategory = async (bookId, newCategory) => {
+    try {
+      await axios.put(`/api/books/${bookId}/`, { status: newCategory })
+      console.log(`Book moved to ${newCategory}`)
+      setBooks((prevBooks) => prevBooks.filter((book) => book.id !== bookId))
+    } catch (error) {
+      console.error(`Error moving to ${newCategory}: `, error)
+    }
+  }
+
+  return (
+    <div>
+      <h1>Currently Reading</h1>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            {book.title} by {book.author}
+            <button onClick={() => handleMoveToCategory(book.id, 'wishlist')}>Move to Wishlist</button>
+            <button onClick={() => handleMoveToCategory(book.id, 'finished')}>Move to Finished</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+
+}
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,1 +1,11 @@
-export default function Home() {}
+import { Link } from "react-router-dom"
+
+export default function Home() {
+  return (
+    <>
+      <h1>bookshelf</h1>
+      <Link to='/wishlist'>Wishlist</Link>
+    </>
+    
+  )
+}
