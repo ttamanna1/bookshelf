8fe1ab439c06f69be30618884b3cf1e5de9b727c
Tamanna <tamannakhanum@hotmail.co.uk>
2024-01-14 14:16:54 +0000

    "Updated routes in main.jsx file."

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,11 +6,65 @@ import './styles/main.scss'
 
 // Page Components
 import App from './App'
+import ErrorPage from './components/ErrorPage'
+import Home from './components/Home'
+import BookIndex from './components/BookIndex'
+import SingleBook from './components/SingleBook'
+import Wishlist from './components/Wishlist'
+import CurrentlyReading from './components/CurrentlyReading'
+import Finished from './components/Finished'
+import CreateBook from './components/CreateBook'
+import EditBook from './components/EditBook'
+import Register from './components/Register'
+import Login from './components/Login'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/books',
+        element: <BookIndex />
+      },
+      {
+        path: '/books/:bookId',
+        element: <SingleBook />
+      },
+      {
+        path: '/wishlist',
+        element: <Wishlist />
+      },
+      {
+        path: '/currently-reading',
+        element: <CurrentlyReading />
+      },
+      {
+        path: '/finished',
+        element: <Finished />
+      },
+      {
+        path: '/books/create',
+        element: <CreateBook />
+      },
+      {
+        path: '/books/:bookId/edit',
+        element: <EditBook />
+      },
+      {
+        path: '/register',
+        element: <Register />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      }
+    ]
   }
 ])
 
