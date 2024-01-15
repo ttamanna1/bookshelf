618c916a79ad271219501cf7fe8acc659366177e
Tamanna <tamannakhanum@hotmail.co.uk>
2024-01-15 22:57:23 +0000

    "Updated App.jsx."

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,26 @@
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
+
+import Spinner from 'react-bootstrap/Spinner'
 
 
 function App() {
   
+  const navigation = useNavigation()  
 
   return (
     <>
       <NavBar />
       <main>
-        <Outlet />
+        {
+          navigation.state === 'idle' ?
+          <Outlet />
+          :
+          <div className="centred">
+            <Spinner animation='border' />
+          </div>
+        }
       </main>
       <Footer />
     </>
