f4ca6b03def7067ecb14f952f0e5497aa53250b1
Tamanna <tamannakhanum@hotmail.co.uk>
2024-01-15 21:00:05 +0000

    "Add page title."

--- client/src/components/BookIndex.jsx
+++ client/src/components/BookIndex.jsx
@@ -1,21 +1,22 @@
-import { useLoaderData } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 export default function BookIndex() {
   const books = useLoaderData()
 
   return (
     <>
-    {books.sort((a, b) => a.title < b.title ? -1 : 1).map(book => {
-          const { id, title, image, author } = book
+      <h1>Books</h1>
+      {books.sort((a, b) => a.title < b.title ? -1 : 1).map(book => {
+          const { id, title, image } = book
           return (
-              <Link key={id} to={`/recipes/${id}`} className="card-layout">
+              <Link key={id} to={`/books/${id}`} className="card-layout">
                 <div className="card" style={{width: '20rem'}}>
                   <img className="card-img-top" src={image} alt={title} style={{height: '150px', objectFit: 'cover'}}/>
-                  <div className="card-body">
+                  {/* <div className="card-body">
                     <h5 className="text-center bold card-title">{title}</h5>
                     <p className="text-center card-text">Author: {author}</p>
-                  </div>
+                  </div> */}
                 </div> 
               </Link>
           )
