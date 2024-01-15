3498352f97cca5782a5997da22a96bf22e4e6259
Tamanna <tamannakhanum@hotmail.co.uk>
2024-01-15 22:10:22 +0000

    "Updated finished component to handle status."

--- client/src/components/Finished.jsx
+++ client/src/components/Finished.jsx
@@ -1,21 +1,46 @@
-import { useLoaderData } from 'react-router-dom'
-import { useState } from 'react'
+
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export default function Finished() {
-  const booklist = useLoaderData()
 
   //! State
-  const [books, setBooks] = useState(booklist || [])
-  
+  const [books, setBooks] = useState( [])
+
+  //! Effects
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/books/?status=finished')
+        if (!response.ok) {
+          throw new Error('Failed to fetch finished data')
+        }
+        const data = await response.json()
+        setBooks(data)
+      } catch (error) {
+        console.error('Error fetching finished list:', error)
+      }
+    }
+    fetchData()
+  }, [])
 
   const handleMoveToCategory = async (bookId, newCategory) => {
     try {
+      setBooks((prevBooks) =>
+        prevBooks.map((book) =>
+          book.id === bookId ? { ...book, status: newCategory } : book
+        )
+      )
       await axios.put(`/api/books/${bookId}/`, { status: newCategory })
       console.log(`Book moved to ${newCategory}`)
-      setBooks((prevBooks) => prevBooks.filter((book) => book.id !== bookId))
+  
     } catch (error) {
       console.error(`Error moving to ${newCategory}: `, error)
+      setBooks((prevBooks) =>
+        prevBooks.map((book) =>
+          book.id === bookId ? { ...book, status: 'wishlist' } : book
+        )
+      )
     }
   }
 
@@ -25,12 +50,13 @@ export default function Finished() {
       <ul>
         {books.map((book) => (
           <li key={book.id}>
-            {book.title} by {book.author}
+            {book.image} 
             <button onClick={() => handleMoveToCategory(book.id, 'wishlist')}>Move to Wishlist</button>
-            <button onClick={() => handleMoveToCategory(book.id, 'curenntly-reading')}>Move to Currently Reading</button>
+            <button onClick={() => handleMoveToCategory(book.id, 'currently-reading')}>Move to Currently Reading</button>
           </li>
         ))}
       </ul>
     </div>
   )
+
 }
