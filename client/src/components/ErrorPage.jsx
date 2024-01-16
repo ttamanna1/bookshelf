b7374453d644caba11c1bcb39ad1845b1a29153a
Tamanna <tamannakhanum@hotmail.co.uk>
2024-01-16 19:47:21 +0000

    "Created error page."

--- client/src/components/ErrorPage.jsx
+++ client/src/components/ErrorPage.jsx
@@ -1,1 +1,19 @@
-export default function ErrorPage() {}
+import { useRouteError } from "react-router-dom"
+import NavBar from "./NavBar"
+import Footer from "./Footer"
+
+export default function ErrorPage() {
+  const error = useRouteError()
+  console.error(error)
+  console.log('Error Status ->', error.status)
+
+  return (
+    <>
+      <NavBar />
+      <div id="error-page">
+        <p>Sorry, an unexpected error has occurred!</p>
+      </div>
+      <Footer />
+    </>
+  )
+}
