2d5ba70545ed872f3086c86b7122d0e9bb50445b
Tamanna <tamannakhanum@hotmail.co.uk>
2024-01-16 09:21:59 +0000

    "Fixed status in views."

--- books/views.py
+++ books/views.py
@@ -9,27 +9,47 @@ from lib.permissions import IsOwnerOrReadOnly
 # PATH: /records/
 # Method: GET, POST
 class BookListCreateView(OwnerListCreateView):
-  queryset = Book.objects.all()
   serializer_class = BookSerializer
   permission_classes = [IsAuthenticatedOrReadOnly]
 
-  def create(self, request, *args, **kwargs):
-    request.data['status'] = 'wishlist'
-    return super().create(request, *args, **kwargs)
+  def get_queryset(self):
+    status = self.request.query_params.get('status', None)
+    
+    if status:
+        # If status is provided, filter by status
+        return Book.objects.filter(status=status)
+    else:
+        # If no status is provided, return all books
+        return Book.objects.all()
+
+  # def create(self, request, *args, **kwargs):
+  #   print('Received data in create:', request.data)
+  #   request.data['status'] = 'wishlist'
+  #   return super().create(request, *args, **kwargs)
+  def perform_create(self, serializer):
+    # Default to 'wishlist' if 'status' is not provided in the request
+    status_value = self.request.data.get('status', 'wishlist')
+    serializer.save(owner=self.request.user, status=status_value)
 
 # PATH: /records/:id
 # Method: GET, PUT, PATCH, DELETE
 class BookDetailView(RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    permission_classes = [IsOwnerOrReadOnly]
+  queryset = Book.objects.all()
+  permission_classes = [IsOwnerOrReadOnly]
 
-    def get_serializer_class(self):
-      print('self request method -->', self.request.method)
-      if self.request.method == 'PUT':
-        return BookSerializer
-      return PopulatedBookSerializer
-    
-    def update(self, request, *args, **kwargs):
-      request.data['status'] = 'wishlist'
-      return super().update(request, *args, **kwargs)
-    
+  def get_serializer_class(self):
+    print('self request method -->', self.request.method)
+    if self.request.method == 'PUT':
+      return BookSerializer
+    return PopulatedBookSerializer
+  
+  def update(self, request, *args, **kwargs):
+    if 'status' in request.data:
+      # Update the status to the provided value
+      request.data['status'] = request.data['status']
+    else:
+        # If 'status' is not present, leave it unchanged
+        request.data['status'] = self.get_object().status
+
+    return super().update(request, *args, **kwargs)
+      
