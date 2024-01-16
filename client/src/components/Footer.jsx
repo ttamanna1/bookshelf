aec0d5b79627b79c90f4c87b8666609cc7431df7
Tamanna <tamannakhanum@hotmail.co.uk>
2024-01-16 21:04:00 +0000

    "Added github link to footer."

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,1 +1,13 @@
-export default function Footer() {}
+import { Link } from 'react-router-dom'
+//Icon
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <Link target='_blank' to='https://github.com/ttamanna1' className='links'>
+      <FontAwesomeIcon icon={faGithub} />Tamanna Khanum</Link>
+    </footer>
+  )
+}
